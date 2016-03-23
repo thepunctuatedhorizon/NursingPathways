2e7044bdf369dfb1282b16f71201eefc61484483
Koby Samuel <ksamuel890@gmail.com>
2016-03-23 18:56:23 -0400

    re-added new registrationdenied class and added layout for said class

--- app/src/main/java/com/jones/android/nursingpathways/RegistrationDenied.java
+++ app/src/main/java/com/jones/android/nursingpathways/RegistrationDenied.java
@@ -1,8 +1,13 @@
 package com.jones.android.nursingpathways;
 
-/**
- * Created by kobes3 on 3/23/16.
- */
-public class RegistrationDenied {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
+public class RegistrationDenied extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_denied);
+    }
 }
